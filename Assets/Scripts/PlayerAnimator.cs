8e3e62dc40d2bc259331faab02cc88950bd5af70
Apurv singh <apurvsingh9713@gmail.com>
2020-10-09 18:57:39 +0530

    Revert "Revert "Add. Inventory UI""
    This reverts commit 0eb429357bd8c14e745f908fac4ead2960761e3b.

---
+++ Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{ 
+
+    protected virtual void Start()
+    {
+    }
+
+    private void Update()
+    {
+    }
+}
---
+++ Assets/Scripts/ScriptableObjects/Item.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/SpawnManagerScriptableObject", order = 1)]
+public class Item : ScriptableObject
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
